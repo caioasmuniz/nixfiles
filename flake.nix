e3c2ba6c18de1d51ec6de9f471fca5f72fc7d466
caioasmuniz <caiomuniz888@gmail.com>
2023-03-31 11:56:51 -0300

    updated flake

--- flake.nix
+++ flake.nix
@@ -3,7 +3,8 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    hyprland.url = "github:hyprwm/Hyprland/37ced6aca4ad8f18f8e205e3cdb6de9c8964d4b6";
+    hyprland.url = "github:hyprwm/Hyprland";
+    fufexan.url = "github:fufexan/dotfiles";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
