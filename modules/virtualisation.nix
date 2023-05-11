69f6f8b8ee11e446fc86308ede66904ff0c230ec
caioasmuniz <caiomuniz888@gmail.com>
2023-05-11 12:05:17 -0300

    updated virtualisation module

--- modules/virtualisation.nix
+++ modules/virtualisation.nix
@@ -1,5 +1,12 @@
 { pkgs, ... }: {
-  virtualisation.libvirtd.enable = true;
+  virtualisation = {
+    libvirtd.enable = true;
+    vmVariant.virtualisation = {
+      memorySize = 4096;
+      cores = 4;
+      qemu.options = [ "-vga qxl" "-device VGA,vgamem_mb=64"];
+    };
+  };
   programs.dconf.enable = true;
   environment.systemPackages = with pkgs; [ virt-manager ];
 }
