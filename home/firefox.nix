4a36b799ed88ee55393f69ca80347da93dc2701a
caioasmuniz <caiomuniz888@gmail.com>
2023-06-16 12:16:30 -0300

    fixed firefox module format, added default search

--- home/firefox.nix
+++ home/firefox.nix
@@ -2,15 +2,16 @@
   home.file."firefox-gnome-theme" = {
     target = ".mozilla/firefox/default/chrome/firefox-gnome-theme";
     source = pkgs.fetchFromGitHub {
-          owner = "rafaelmardojai";
-          repo = "firefox-gnome-theme";
-          rev = "v113";
-          sha256 = "c1TTeZUVI4FPSTGJPBucELnzYr96IF+g++9js3eJvm8=";
-        };
+      owner = "rafaelmardojai";
+      repo = "firefox-gnome-theme";
+      rev = "v113";
+      sha256 = "c1TTeZUVI4FPSTGJPBucELnzYr96IF+g++9js3eJvm8=";
+    };
   };
   programs.firefox = {
     enable = true;
     profiles.default = {
+      search.default = "DuckDuckGo";
       settings = {
         "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
         "svg.context-properties.content.enabled" = true;
