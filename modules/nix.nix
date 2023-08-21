2cdb60b85c066c929564db4ad5b465ac864986d2
caioasmuniz <caiomuniz888@gmail.com>
2023-08-21 16:55:32 -0300

    nix: remove hyprland cachix, add store optimise

--- modules/nix.nix
+++ modules/nix.nix
@@ -7,10 +7,9 @@
   };
 
   nix.settings = {
+    auto-optimise-store = true;
     keep-outputs = true;
     keep-derivations = true;
-    substituters = [ "https://hyprland.cachix.org" ];
-    trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
     experimental-features = [ "nix-command" "flakes" ];
   };
 }
