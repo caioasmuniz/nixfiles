cd77a77231d4068267c099f6c99ca8d7dba88339
caioasmuniz <caiomuniz888@gmail.com>
2023-08-21 16:49:39 -0300

    swayosd: use nixpkgs module, updated configs

--- home/swayosd.nix
+++ home/swayosd.nix
@@ -1,12 +1,11 @@
-{ pkgs, ... }: {
-  home.packages = [ pkgs.swayosd ];
+{ ... }: {
+  services.swayosd.enable = true;
   wayland.windowManager.hyprland.extraConfig = ''
-    exec-once=swayosd
-    bindle=, XF86AudioRaiseVolume, exec, swayosd --output-volume raise
-    bindle=, XF86AudioLowerVolume, exec, swayosd --output-volume lower
+    bindle=, XF86AudioRaiseVolume, exec, swayosd --output-volume raise 5
+    bindle=, XF86AudioLowerVolume, exec, swayosd --output-volume lower 5
 
-    bindle=SHIFT, XF86AudioRaiseVolume, exec, swayosd --input-volume raise
-    bindle=SHIFT, XF86AudioLowerVolume, exec, swayosd --input-volume lower
+    bindle=SHIFT, XF86AudioRaiseVolume, exec, swayosd --input-volume raise 5
+    bindle=SHIFT, XF86AudioLowerVolume, exec, swayosd --input-volume lower 5
 
     bind=, XF86AudioMute, exec, swayosd --output-volume mute-toggle
     bind=, XF86AudioMicMute, exec, swayosd --input-volume mute-toggle
@@ -15,7 +14,7 @@
     binde=, XF86MonBrightnessDown, exec, light -U 10
 
     bindl=, Caps_Lock, exec, swayosd --caps-lock-led input0::capslock
-    binde=, XF86MonBrightnessUp, exec, swayosd --brightness raise
-    binde=, XF86MonBrightnessDown, exec, swayosd --brightness lower
+    binde=, XF86MonBrightnessUp, exec, swayosd --brightness raise 5
+    binde=, XF86MonBrightnessDown, exec, swayosd --brightness lower 5
   '';
 }
