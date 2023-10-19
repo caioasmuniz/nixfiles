212c0ec7a172edc21a19179563ce063b1bbe829d
caioasmuniz <caiomuniz888@gmail.com>
2023-10-19 14:18:31 -0300

    updated darkman module to nixpkgs one, bump flake

--- home/darkman.nix
+++ home/darkman.nix
@@ -1,70 +1,38 @@
 { pkgs, ... }: {
-  home.packages = [ pkgs.darkman ];
-  systemd.user.services.darkman = {
-    Unit = {
-      Description = "Framework for dark-mode and light-mode transitions.";
-      Documentation = "man:darkman(1)";
+  services.darkman = {
+    enable = true;
+    settings = {
+      lat = -23.1;
+      lng = -50.6;
+      dbusserver = true;
+      usegeoclue = false;
+      portal = true;
     };
-    Service = {
-      Type = "dbus";
-      ExecStart = "${pkgs.darkman}/bin/darkman run";
-      Restart = "on-failure";
-      Environment = "PATH=/run/current-system/sw/bin";
-      TimeoutStopSec = 15;
-      Slice = "background.slice";
-      BusName = "nl.whynothugo.darkman";
-      RestrictNamespaces = "yes";
-      SystemCallArchitectures = "native";
-      SystemCallFilter = "@system-service @timer";
-      MemoryDenyWriteExecute = "yes";
-    };
-    Install.WantedBy = [ "graphical-session.target" ];
-  };
-  xdg = {
-    configFile."darkman/config.yaml".text = ''
-      lat: -23.1
-      lng: -50.6
-      dbusserver: true
-      usegeoclue: false
-      portal: true
+    lightModeScripts.light = ''
+      ${pkgs.swaybg}/bin/swaybg -m fill -i ~/Pictures/bigsur-day.jpg &
+      
+      rm ~/.config/gtklock/config.ini; echo -e "[main]\ngtk-theme=Adwaita" >>.config/gtklock/config.ini
+      
+      ${pkgs.dconf}/bin/dconf write /org/gnome/desktop/interface/gtk-theme '"Adwaita"'
+      ${pkgs.dconf}/bin/dconf write /org/gnome/desktop/interface/color-scheme '"prefer-light"'
+
+      ln -sf ~/.config/kitty/themes/adwaita.conf ~/.config/kitty/current-theme.conf
+      pkill -USR1 kitty
+
+      ${pkgs.libnotify}/bin/notify-send --app-name="darkman" --urgency=low --icon=weather-clear-symbolic "switching to light mode"
     '';
-
-    dataFile = {
-      "light-mode.d/light.sh" = {
-        executable = true;
-        text = ''
-          ${pkgs.swaybg}/bin/swaybg -m fill -i ~/Pictures/bigsur-day.jpg &
+    darkModeScripts.dark = ''
+      ${pkgs.swaybg}/bin/swaybg -m fill -i ~/Pictures/bigsur-night.jpg &
       
-          rm ~/.config/gtklock/config.ini; echo -e "[main]\ngtk-theme=Adwaita" >>.config/gtklock/config.ini
+      rm ~/.config/gtklock/config.ini; echo -e "[main]\ngtk-theme=Adwaita-dark" >>.config/gtklock/config.ini
       
-          ${pkgs.dconf}/bin/dconf write /org/gnome/desktop/interface/gtk-theme '"Adwaita"'
+      ${pkgs.dconf}/bin/dconf write /org/gnome/desktop/interface/gtk-theme '"Adwaita-dark"'
+      ${pkgs.dconf}/bin/dconf write /org/gnome/desktop/interface/color-scheme '"prefer-dark"'      
+
+      ln -sf ~/.config/kitty/themes/adwaita-dark.conf ~/.config/kitty/current-theme.conf
+      pkill -USR1 kitty
       
-          ${pkgs.dconf}/bin/dconf write /org/gnome/desktop/interface/color-scheme '"prefer-light"'
-
-          ln -sf ~/.config/kitty/themes/adwaita.conf ~/.config/kitty/current-theme.conf
-          pkill -USR1 kitty
-
-          ${pkgs.libnotify}/bin/notify-send --app-name="darkman" --urgency=low --icon=weather-clear-symbolic "switching to light mode"
-        '';
-      };
-
-      "dark-mode.d/dark.sh" = {
-        executable = true;
-        text = ''
-          ${pkgs.swaybg}/bin/swaybg -m fill -i ~/Pictures/bigsur-night.jpg &
-      
-          rm ~/.config/gtklock/config.ini; echo -e "[main]\ngtk-theme=Adwaita-dark" >>.config/gtklock/config.ini
-      
-          ${pkgs.dconf}/bin/dconf write /org/gnome/desktop/interface/gtk-theme '"Adwaita-dark"'
-      
-          ${pkgs.dconf}/bin/dconf write /org/gnome/desktop/interface/color-scheme '"prefer-dark"'
-
-          ln -sf ~/.config/kitty/themes/adwaita-dark.conf ~/.config/kitty/current-theme.conf
-          pkill -USR1 kitty
-
-          ${pkgs.libnotify}/bin/notify-send --app-name="darkman" --urgency=low --icon=weather-clear-night-symbolic "switching to dark mode"
-        '';
-      };
-    };
+      ${pkgs.libnotify}/bin/notify-send --app-name="darkman" --urgency=low --icon=weather-clear-night-symbolic "switching to dark mode"
+    '';
   };
 }
