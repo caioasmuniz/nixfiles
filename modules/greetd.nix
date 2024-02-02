83be0dea8bdc54390f8f4fe79e0d12c89e9d284e
caioasmuniz <caiomuniz888@gmail.com>
2024-02-02 11:45:51 -0300

    simplified greetd config: - now using the default cage compositor

--- modules/greetd.nix
+++ modules/greetd.nix
@@ -1,33 +1,14 @@
-{ pkgs, ... }:
-let
-  swayConfig = pkgs.writeText "greetd-sway-config" ''
-    exec "dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY SWAYSOCK XDG_CURRENT_DESKTOP"
-    exec "${pkgs.darkman}/bin/darkman run &"
-    input "type:touchpad" {
-      tap enabled
-    }
-    xwayland disable
-    exec "${pkgs.greetd.regreet}/bin/regreet; swaymsg exit"
-  '';
-in
-{
+{ pkgs, ... }: {
   security.pam.services.greetd.enableGnomeKeyring = true;
-
-  services.greetd = {
-    enable = true;
-    settings.default_session.command = "${pkgs.sway}/bin/sway --config ${swayConfig}";
-  };
-
   environment.systemPackages = [ pkgs.gnome.adwaita-icon-theme ];
 
   programs.regreet = {
     enable = true;
-    settings = {
-      GTK = {
-        cursor_theme_name = "Adwaita";
-        font_name = "Fira Sans 12";
-        icon_theme_name = "Adwaita";
-      };
+    cageArgs = [ "-s" "-d" "-m" "last" ];
+    settings.GTK = {
+      cursor_theme_name = "Adwaita";
+      font_name = "Fira Sans 12";
+      icon_theme_name = "Adwaita";
     };
   };
 }
