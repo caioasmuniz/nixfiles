80cba9aff246465031d9e82acc628e61eeceffcb
caioasmuniz <caiomuniz888@gmail.com>
2024-06-02 14:49:19 -0300

    fixed osu

--- home/gaming.nix
+++ home/gaming.nix
@@ -1,7 +1,6 @@
 { inputs, pkgs, ... }: {
   home.packages = with pkgs; [
     bottles
-   ( inputs.nix-gaming.packages.${pkgs.system}.osu-lazer-bin.override
-    { gmrun_enable = false; })
+    osu-lazer-bin
   ];
 }
