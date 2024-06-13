6c688ea01ca6cc890b14c5e94c52a54f359adef8
caioasmuniz <caiomuniz888@gmail.com>
2024-06-13 15:25:50 -0300

    refactored aspire specif config

--- hosts/aspire/default.nix
+++ hosts/aspire/default.nix
@@ -1,20 +1,43 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ config, ... }: {
+{ config, pkgs, ... }: {
   imports = [ ../../modules ./hardware.nix ];
-  networking.hostName = "aspire";
-  hardware.nvidia = {
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
-    open = false;
-    modesetting.enable = true;
-    powerManagement.enable = false;
-    prime = {
-      sync.enable = true;
-      nvidiaBusId = "PCI:0:1:0";
-      intelBusId = "PCI:0:0:2";
+  networking = {
+    hostName = "aspire";
+    interfaces.enp3s0.wakeOnLan.enable = true;
+  };
+  hardware = {
+    opengl = {
+      enable = true;
+      extraPackages = with pkgs; [
+        nvidia-vaapi-driver
+        intel-media-driver
+        (vaapiIntel.override { enableHybridCodec = true; })
+        vaapiVdpau
+        libvdpau-va-gl
+        intel-compute-runtime
+      ];
+    };
+    nvidia = {
+      package = config.boot.kernelPackages.nvidiaPackages.beta;
+      open = false;
+      modesetting.enable = true;
+      powerManagement.enable = false;
+      prime = {
+        sync.enable = true;
+        nvidiaBusId = "PCI:0:1:0";
+        intelBusId = "PCI:0:0:2";
+      };
     };
   };
-  services.xserver.videoDrivers = [ "nvidia" ];
+
+  services = {
+    xserver.videoDrivers = [ "nvidia" ];
+    sunshine = {
+      enable = true;
+      package = pkgs.sunshine.override { cudaSupport = true; };
+      autoStart = true;
+      capSysAdmin = true;
+      openFirewall = true;
+    };
+  };
+  environment.systemPackages = [ pkgs.nvtop pkgs.egl-wayland ];
 }
