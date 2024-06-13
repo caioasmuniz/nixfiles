7ec558e8110239b2ce28245c68a1e0f5c0814d52
caioasmuniz <caiomuniz888@gmail.com>
2024-06-13 15:22:31 -0300

    small ssh fixes

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -7,12 +7,12 @@
       StreamLocalBindUnlink = "yes";
       GatewayPorts = "clientspecified";
     };
-
     hostKeys = [{
       path = "/etc/ssh/ssh_host_ed25519_key";
       type = "ed25519";
     }];
   };
+  users.users.caio.openssh.authorizedKeys.keyFiles = ../hosts/inspiron/ssh_host_ed25519_key.pub;
 
   programs.ssh = {
     # Each hosts public key
