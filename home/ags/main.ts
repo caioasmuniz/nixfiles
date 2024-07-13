896b5429fb1ab654252bd1cc325fd1bae36ecaa5
caioasmuniz <caiomuniz888@gmail.com>
2024-07-13 20:30:39 -0300

    updated ags fixed bar being show multiple times

--- home/ags/main.ts
+++ home/ags/main.ts
@@ -2,12 +2,14 @@ import bar from "./widget/bar/bar.js";
 import applauncher from "./widget/applauncher.js";
 import quicksettings from "widget/quicksettings/quicksettings.js";
 import notificationPopup from "./widget/notifications/notificationPopup.js";
+import { forMonitors } from "lib/utils.js";
+
+const hyprland = await Service.import("hyprland");
 
 App.config({
   style: "./style.css",
   windows: [
-    bar(0),
-    bar(1),
+    ...forMonitors(bar),
     applauncher(),
     quicksettings(),
     notificationPopup(),
