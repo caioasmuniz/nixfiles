cf3580e97037066aaa922b12b7b4012bf8dbd2fe
caioasmuniz <caiomuniz888@gmail.com>
2025-08-04 16:32:27 -0300

    add ollama unified memory config

--- modules/ollama.nix
+++ modules/ollama.nix
@@ -7,6 +7,9 @@
       openFirewall = true;
       host = "0.0.0.0";
       acceleration = "cuda";
+      environmentVariables = {
+      	GGML_CUDA_ENABLE_UNIFIED_MEMORY = "1";
+      };
     };
   };
 }
